23589ec26b98f375eb1cb43890017654c6d80fce
allmuniz <146678270+allmuniz@users.noreply.github.com>
2023-12-28 13:46:49 -0300

    Create functions

--- 00 - Principal/desafios/fontend_mentor/D-001/script/script.js
+++ 00 - Principal/desafios/fontend_mentor/D-001/script/script.js
@@ -1,4 +1,29 @@
 var description = document.querySelector('div#txt1')
 function openparagraph(){
-    if()
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    }else{
+        itens.style.display = 'block'
+    }
+}
+function openparagraph2(){
+    if(itens2.style.display == 'block'){
+        itens2.style.display = 'none'
+    }else{
+        itens2.style.display = 'block'
+    }
+}
+function openparagraph3(){
+    if(itens3.style.display == 'block'){
+        itens3.style.display = 'none'
+    }else{
+        itens3.style.display = 'block'
+    }
+}
+function openparagraph4(){
+    if(itens4.style.display == 'block'){
+        itens4.style.display = 'none'
+    }else{
+        itens4.style.display = 'block'
+    }
 }
